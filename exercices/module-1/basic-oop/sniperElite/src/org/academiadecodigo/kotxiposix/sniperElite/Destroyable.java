ecb704cece8659392de0ba175580fc6479832732
Rony Gomes <gromilton7@gmail.com>
2021-01-07 12:06:54 -0100

    create Destroyable Interface

---
+++ exercices/module-1/basic-oop/sniperElite/src/org/academiadecodigo/kotxiposix/sniperElite/Destroyable.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.kotxiposix.sniperElite;
+
+public interface Destroyable {
+
+
+    public abstract void hit(int damage);
+    public abstract boolean isDestroyed();
+
+}
