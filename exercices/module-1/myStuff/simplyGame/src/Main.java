7fdd72490b486babc9c68442cd300a2995667660
Rony Gomes <gromilton7@gmail.com>
2021-01-08 13:00:43 -0100

    added car-crash-x-skeleton

---
+++ exercices/module-1/myStuff/simplyGame/src/Main.java
@@ -0,0 +1,14 @@
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(20, 20, 50, 50);
+        rectangle.setColor(Color.DARK_GRAY);
+        rectangle.fill();
+
+
+    }
+}
