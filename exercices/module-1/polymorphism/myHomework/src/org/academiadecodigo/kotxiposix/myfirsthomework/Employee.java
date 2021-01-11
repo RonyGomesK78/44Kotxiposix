5ec8264682bcbd70e5c81c0976e0870576679638
Rony Gomes <gromilton7@gmail.com>
2021-01-11 19:07:45 -0100

    created Employee and EmployeeType class

---
+++ exercices/module-1/polymorphism/myHomework/src/org/academiadecodigo/kotxiposix/myfirsthomework/Employee.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.kotxiposix.myfirsthomework;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private EmployeeType employeeType;
+    private double salary;
+}
---
+++ exercices/module-1/polymorphism/myHomework/src/org/academiadecodigo/kotxiposix/myfirsthomework/EmployeeType.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.kotxiposix.myfirsthomework;
+
+public enum EmployeeType {
+
+    TEACHER,
+    STUDENT,
+    SECRETARY
+}
