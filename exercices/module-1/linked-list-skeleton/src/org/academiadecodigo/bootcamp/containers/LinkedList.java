5e089ef939ef80bc12f8cf93382f8a58f3bba17d
Rony Gomes <gromilton7@gmail.com>
2021-01-13 18:42:35 -0100

    4 ok's

---
+++ exercices/module-1/linked-list-skeleton/src/org/academiadecodigo/bootcamp/containers/LinkedList.java
@@ -0,0 +1,148 @@
+package org.academiadecodigo.bootcamp.containers;
+
+public class LinkedList {
+
+    private Node head;
+    private int length = 0;
+
+    public LinkedList() {
+
+        this.head = new Node(null);
+    }
+
+    public int size() {
+
+        return length;
+    }
+
+    /**
+     * Adds an element to the end of the list
+     * @param data the element to add
+     */
+    public void add(Object data)  {
+
+        Node node = new Node(data);
+        Node iterator = head;
+        while (iterator.getNext() != null){
+            iterator = iterator.getNext();
+        }
+        iterator.setNext(node);
+        length++;
+
+    }
+
+    /**
+     * Obtains an element by index
+     * @param index the index of the element
+     * @return the element
+     */
+    public Object get(int index) {
+
+
+        Node iterator = this.head.getNext();
+
+        while (iterator != null){
+
+            if (index == 0){
+
+                return iterator.getData();
+            }
+            iterator = iterator.getNext();
+            index--;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the index of the element in the list
+     * @param data element to search for
+     * @return the index of the element, or -1 if the list does not contain element
+     */
+    public int indexOf(Object data) {
+
+        Node iterator = this.head;
+
+        for (int i = 0; i < length; i++) {
+
+            iterator = iterator.getNext();
+
+            if (iterator.getData().equals(data)){
+
+                return  i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Removes an element from the list
+     * @param data the element to remove
+     * @return true if element was removed
+     */
+    public boolean remove(Object data) {
+
+        Node iterator = this.head.getNext();
+
+
+        if (iterator != null &&  data.equals(iterator.getData())){
+
+            head.setNext(iterator.getNext());
+            length--;
+
+            return true;
+        }
+
+        Node followIterator = head;
+
+        while (iterator != null){
+
+            if (data.equals(iterator.getData())){
+
+                followIterator.setNext(iterator.getNext());
+
+                length--;
+
+                return true;
+            }
+
+            followIterator = iterator;
+            iterator = iterator.getNext();
+        }
+
+        return false;
+    }
+
+    private class Node {
+
+        private Object data;
+        private Node next;
+
+        public Node(Object data) {
+            this.data = data;
+            next = null;
+        }
+
+        public Object getData() {
+
+            return data;
+        }
+
+        public void setData(Object data) {
+
+            this.data = data;
+        }
+
+        public Node getNext() {
+
+            return next;
+        }
+
+        public void setNext(Node next) {
+
+            this.next = next;
+        }
+    }
+
+}
