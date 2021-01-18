e7c276a968a2811277cceabcb65c8ee46ac6b75a
Rony Gomes <gromilton7@gmail.com>
2021-01-18 19:37:19 -0100

    add histogram exercise using hash map with composition

--- exercices/module-1/histogram/src/org/academiadecodigo/kotxiposix/histogram/Histogram.java
+++ exercices/module-1/histogram/src/org/academiadecodigo/kotxiposix/histogram/Histogram.java
@@ -1,8 +1,47 @@
 package org.academiadecodigo.kotxiposix.histogram;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class Histogram {
+public class Histogram implements Iterable<String>{
 
-    LinkedHashMap<String, int> hashMap;
+    private Map<String, Integer> hashMap;
+
+    @Override
+    public Iterator<String> iterator() {
+
+        return hashMap.keySet().iterator();
+    }
+
+    public Histogram(String sentence){
+
+        hashMap = new HashMap<>();
+
+        String[] words = splitSentence(sentence);
+
+        for (String word : words) {
+
+            if(hashMap.containsKey(word)){
+
+                hashMap.put(word, hashMap.get(word) + 1);
+            }
+            else {
+
+                hashMap.put(word, 1);
+            }
+        }
+    }
+
+    public int getValue(String word){
+
+        return hashMap.get(word);
+    }
+    public String[] splitSentence(String sentence){
+
+        return sentence.split(" ");
+    }
+
+
 }
--- exercices/module-1/histogram/src/org/academiadecodigo/kotxiposix/histogram/Main.java
+++ exercices/module-1/histogram/src/org/academiadecodigo/kotxiposix/histogram/Main.java
@@ -1,2 +1,14 @@
-package org.academiadecodigo.kotxiposix.histogram;public class Main {
+package org.academiadecodigo.kotxiposix.histogram;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Histogram histogram = new Histogram("Hello Samuel Samuela Rony Hello Is It Me Me Samuel");
+
+        for (String word : histogram) {
+
+            System.out.println(word + " : " + histogram.getValue(word));
+        }
+    }
 }
